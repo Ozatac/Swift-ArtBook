4a35b09400386716e56c5bd7efc49fb897051230
Tunahan Ozatac <ExtTunahanO@d-teknoloji.com.tr>
2022-01-08 17:07:19 +0300

    Initial Commit

---
+++ ArtBookUITests/ArtBookUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ArtBookUITestsLaunchTests.swift
+//  ArtBookUITests
+//
+//  Created by Tunahan Ozatac (Berkut Teknoloji) on 8.01.2022.
+//
+
+import XCTest
+
+class ArtBookUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
