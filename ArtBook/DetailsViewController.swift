0ef64ee1278503e6e779cf44e126657386ad84b2
Tunahan Ozatac <ExtTunahanO@d-teknoloji.com.tr>
2022-02-06 15:19:31 +0300

    first commit

---
+++ ArtBook/DetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsViewController.swift
+//  ArtBook
+//
+//  Created by Tunahan Ozatac (Berkut Teknoloji) on 8.01.2022.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
